6e052753c560613256b5a866c2798309cbfc52d8
Karol Schweitzer <karol.schweitzer@gmail.com>
2017-06-30 23:44:51 +0200

    Wykonana podstawa strony razem z JSem

--- js/app.js
+++ js/app.js
@@ -5,6 +5,14 @@ document.addEventListener("DOMContentLoaded",function () {
     let meter = null;
     let rafID = null;
     let mediaStreamSource = null;
+    let noMic = document.querySelector(".no_mic");
+    let degrees = 0;
+    let styleElem = document.head.appendChild(document.createElement("style"));
+    let currentHeight = 15;
+
+    document.querySelector(".shroud").addEventListener("mousemove", function(event){    
+        styleElem.innerHTML = ".shroud:after {left: "+event.clientX+"px; top: "+event.clientY+"px;}"
+    })
 
     // Retrieve AudioContext with all the prefixes of the browsers
     window.AudioContext = window.AudioContext || window.webkitAudioContext;
@@ -16,7 +24,7 @@ document.addEventListener("DOMContentLoaded",function () {
      * Callback triggered if the microphone permission is denied
      */
     function onMicrophoneDenied() {
-        alert('Stream generation failed.');
+        noMic.style.display = "block";
     }
 
     /**
@@ -37,21 +45,66 @@ document.addEventListener("DOMContentLoaded",function () {
      * This function is executed repeatedly
      */
     function onLevelChange(time) {
-        // check if we're currently clipping
-
-        // if (meter.checkClipping()) {
-        //     console.warn(meter.volume);
-        // } else {
-        //     console.log(meter.volume);
-        // }
-
         // set up the next callback
         rafID = window.requestAnimationFrame(onLevelChange);
 
-        let degrees = -(meter.volume * 10000)
-        document.querySelector(".windmil_pic").style.transform = "rotate("+degrees+"deg)"
+        let mv = meter.volume
+        //console.log(mv);
+        degrees = degrees + ~~(mv * 100);
+        rotate(degrees);
+        powerGaugeChange(mv);
     }
 
+     function rotate(rotation) {
+        document.querySelector(".windmil_pic").style.transform = "rotate("+rotation+"deg)";
+    }
+
+    let c = document.getElementById("power_gauge");
+    let ctx = c.getContext("2d");
+
+    // Create gradient
+    let grd = ctx.createLinearGradient(0,300,0,0);
+    grd.addColorStop(0.15,"darkred");
+    grd.addColorStop(0.2,"red");
+    grd.addColorStop(0.5,"orange");
+    grd.addColorStop(0.8,"darkgreen");
+    grd.addColorStop(1,"lightgreen");
+    // Fill with gradient
+    ctx.fillStyle = grd;
+    
+    function powerGaugeChange(number) {
+        if (currentHeight <= 0) {
+             return false;
+         }
+        if (number < 0.4) {
+            number = 0;
+        }
+        currentHeight = currentHeight + number;
+        console.log(currentHeight);
+        if (currentHeight >= 280) {
+            currentHeight = 281;
+        }
+        opacity = 1-((1/2.8)*currentHeight)/100;
+        document.querySelector(".shroud").style.opacity = opacity;
+        numberToIncrease = -(currentHeight);
+        ctx.clearRect(0,0,100,300)
+        ctx.fillRect(10,295,80,numberToIncrease);
+    }
+
+    powerInterval = setInterval( () => {
+        currentHeight = currentHeight - 0.5;
+        if (currentHeight <= 0 || currentHeight >= 280) {
+             clearInterval(powerInterval);
+        }
+        opacity = 1-((1/2.8)*currentHeight)/100;
+        document.querySelector(".shroud").style.opacity = opacity;
+        change(currentHeight)
+    },100);
+    
+    function change(number){
+        ctx.clearRect(0,0,100,300)
+        ctx.fillRect(10,280,80,-number);
+    }    
 
     // Try to get access to the microphone
     try {
@@ -78,6 +131,4 @@ document.addEventListener("DOMContentLoaded",function () {
     } catch (e) {
         alert('getUserMedia threw exception :' + e);
     }
-
-
-})
+});
